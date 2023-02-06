775d4ec94f10ce3d658785a665f0bf8ba35ccdcf
Antoine Wauthier <69273386+AntoineWauthier@users.noreply.github.com>
2023-02-06 13:43:45 +0100

    Create player.cpp

---
+++ player.cpp
@@ -0,0 +1,20 @@
+class Player{
+  public:
+  
+  void set_username(string name){
+    username = name
+  }
+  string get_username(){
+    return username;
+  }
+  void set_rank(int newRank){
+    rank = newRank;
+  }
+  int get_rank(){
+    return rank;
+  }
+   
+  private:
+    string username;
+    int rank;
+}
