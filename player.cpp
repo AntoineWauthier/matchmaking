d0a409817d4b74d6583d50ca7e3d5460b3fab409
Antoine Wauthier <69273386+AntoineWauthier@users.noreply.github.com>
2023-02-07 15:25:11 +0100

    Update player.cpp

--- player.cpp
+++ player.cpp
@@ -1,7 +1,7 @@
 class Player{
   public:
-  void set_username(string name){
-    username = name;
+  void set_username(string newName){
+    username = newName;
   }
   string get_username(){
     return username;
@@ -12,8 +12,12 @@ class Player{
   int get_rank(){
     return rank;
   }
-  
+  float get_winRatio(){
+  return wins/(wins+losses);
+  }
   private:
     string username;
     int rank;
+    int wins;
+    int losses;
 }
